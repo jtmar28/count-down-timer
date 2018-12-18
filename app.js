2f7ce08f82c2c7d0ceb89763ce4929b6b7a9ea0a
jtmar28 <justinmartinez0123@gmail.com>
2018-12-17 21:54:12 -0500

    Stop timer when it reaches End time

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const timerDayEl = document.querySelector('timer__day');
-const timerHourEl = document.querySelector('timer__hour');
-const timerMinEl = document.querySelector('timer__min');
-const timerSecEl = document.querySelector('timer__sec');
+const timerDayEl = document.querySelector('.timer__day');
+const timerHourEl = document.querySelector('.timer__hour');
+const timerMinEl = document.querySelector('.timer__min');
+const timerSecEl = document.querySelector('.timer__sec');
 
 function getTimeDifference(start,end){
     let miliseconds = Math.floor(end - start);
@@ -19,15 +19,34 @@ function getTimeDifference(start,end){
         rMinutes: minutes,
         rSeconds : seconds
     }
-
 }
 
 let timer = setInterval(function(){
     const startDate = new Date();
-    const endDate = new Date('Jan 1, 2019');
+    const endDate = new Date('Dec 17, 2018 21:53:19');
     let timeDifference = getTimeDifference(startDate, endDate);
-    timerDayEl.textContent = timeDifference.rDays;
-    timerHourEl.textContent = timeDifference.rHours;
-    timerMinEl.textContent = timeDifference.rMinutes;
-    timerSecEl.textContent = timeDifference.rSeconds;
+    if(timeDifference.rDays.toString().length === 1){
+        timerDayEl.textContent = `0${timeDifference.rDays}`;
+    }else{
+        timerDayEl.textContent = timeDifference.rDays;
+    }
+    if(timeDifference.rHours.toString().length === 1){
+        timerHourEl.textContent = `0${timeDifference.rHours}`;
+    }else{
+        timerHourEl.textContent = timeDifference.rHours;
+    }
+    if(timeDifference.rMinutes.toString().length === 1){
+        timerMinEl.textContent = `0${timeDifference.rMinutes}`;
+    }
+    else{
+        timerMinEl.textContent = timeDifference.rMinutes;
+    }
+    if(timeDifference.rSeconds.toString().length === 1){
+        timerSecEl.textContent = `0${timeDifference.rSeconds}`;
+    }else{
+        timerSecEl.textContent = timeDifference.rSeconds;
+    }
+    if(timeDifference.rDays === 0 && timeDifference.rHours === 0 && timeDifference.rMinutes ===0 && timeDifference.rSeconds ===0){
+        clearInterval(timer);
+    }
 },1000)
